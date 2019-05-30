bad9d76b35a3f82183df2930cf7a49f982d9b974
Guillermo598 <38139707+Guillermo598@users.noreply.github.com>
2019-05-30 11:05:16 -0500

    Delete DataReportController.java

--- dummy_code/src/main/java/com/example/demo/api/DataReportController.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.api;
-
-public class DataReportController {
-    public static DataReportController getInstance() {}
-    private DataReportController(){}
-
-    public void sendReport() {}
-    public void setFrequency() {}
-}
