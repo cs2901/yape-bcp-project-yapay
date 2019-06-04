9db3d8abe45eb2c8da5264fa0fbb92b76f85de7b
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2019-06-04 18:21:34 -0500

    Kill app

--- app/demo/platforms/ios/build/demo.build/Debug-iphonesimulator/demo.build/Objects-normal/i386/NativeScriptEmbedder.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/alessiayi/Documents/NatieScript/barcodedemo/demo/platforms/ios/demo/demo-Prefix.pch \
-  /Users/alessiayi/Documents/NatieScript/barcodedemo/demo/node_modules/tns-core-modules/platforms/ios/src/NativeScriptEmbedder.m \
-  /Users/alessiayi/Documents/NatieScript/barcodedemo/demo/node_modules/tns-core-modules/platforms/ios/src/NativeScriptEmbedder.h
